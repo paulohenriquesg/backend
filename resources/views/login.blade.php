b395e827c8d452ad3c5e9ab7d51b3378a7b9fb29
Michael Kozii <michael.koziy@gmail.com>
2025-05-04 22:32:28 +0200

    Fix title of a login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Login | Photos Nest</title>
+        <title>Login | {{ config('app.name') }}</title>
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
@@ -17,7 +17,7 @@
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <div class="text-center lg:text-left lg:ml-8">
                     <h1 class="text-5xl font-bold">Login</h1>
-                    <p class="py-6">Access your {{ config('app.name') }} account to manage your photos and memories safely in the cloud.</p>
+                    <p class="py-6">Access your {{ config('app.name') }} account to backup your photos and memories safely.</p>
                 </div>
                 <div class="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div class="card-body">
